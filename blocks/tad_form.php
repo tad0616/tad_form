5253acf5fa184004745a9d96285d54e9895f48e1
Tad <tad0616@gmail.com>
2014-01-29 14:13:46 +0800

    修正通知訊息

--- blocks/tad_form.php
+++ blocks/tad_form.php
@@ -14,6 +14,7 @@ function tad_form($options){
 	$sql = "select * from ".$xoopsDB->prefix("tad_form_main")." where enable='1' and start_date < '{$today}' and end_date > '{$today}' limit 0,{$options[1]}";
 	$result = $xoopsDB->query($sql);
   $i=0;
+  $block="";
 	while(list($ofsn,$title,$start_date,$end_date,$content,$uid,$post_date,$enable)=$xoopsDB->fetchRow($result)){
 		//$block.="<a href='".XOOPS_URL."/modules/tad_form/index.php?op=sign&ofsn=$ofsn' >$title</a>";
 		$block['form'][$i]['ofsn']=$ofsn;
