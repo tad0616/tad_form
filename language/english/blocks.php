a7364e7304265f686387410815fb64a3ecdbf483
mambax7 <mambax7@gmail.com>
2015-06-01 07:21:50 -0400

    English Update

--- language/english/blocks.php
+++ language/english/blocks.php
@@ -5,7 +5,6 @@
 // $Id: function.php,v 1.1 2008/05/14 01:22:08 tad Exp $
 // ------------------------------------------------------------------------- //
 
-define("_MB_TADFORM_ONE_FORM_T1","Select Form");
-define("_MB_TADFORM_SIGN_DATE","Date： %s to %s");
-define("_MB_TADFORM_SIGN_NOW","Click Here for 「%s」（Total: %s ）");
-?>
+define('_MB_TADFORM_ONE_FORM_T1', 'Select Form');
+define('_MB_TADFORM_SIGN_DATE', 'Date： %s to %s');
+define('_MB_TADFORM_SIGN_NOW', 'Click Here for "%s"（Total: %s ）');
