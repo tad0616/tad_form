b88aaced9f06769872b7a9198c8a0aa7651bf68f
Tad <tad0616@gmail.com>
2019-04-11 11:01:43 +0800

    「列出指定的線上表單」區塊新增直接顯示表單的功能

--- language/tchinese_utf8/modinfo.php
+++ language/tchinese_utf8/modinfo.php
@@ -11,7 +11,7 @@ define('_MI_TADFORM_ADMENU3', '資料庫架構更新');
 define('_MI_TADFORM_BNAME1', '最新線上表單');
 define('_MI_TADFORM_BDESC1', '列出目前執行中的線上表單');
 define('_MI_TADFORM_BNAME2', '線上表單');
-define('_MI_TADFORM_BDESC2', '列出指定的上表單');
+define('_MI_TADFORM_BDESC2', '列出指定的線上表單');
 
 define('_MI_TADFORM_SHOW_AMOUNT', '前台是否顯示已有幾人填寫');
 define('_MI_TADFORM_SHOW_AMOUNT_DESC', '前台的填報按鈕上是否顯示已有幾人填寫？');
